db61ed974ee78531795d3a21e8fb2d9b12a01403
diy <skxe07@163.com>
2016-08-29 11:41:51 +0800

    首次提交到git仓库，测试一发！

---
+++ src/com/scjsbb/HelloWorld.java
@@ -0,0 +1,33 @@
+package com.scjsbb;
+
+/**
+ * 测试GIT的使用方法
+ * @author DIY
+ * </br>
+ * 创建时间：2016年8月29日上午11:35:57
+ *
+ */
+public class HelloWorld {
+	
+	/**
+	 * 测试主方法
+	 * @author diy
+	 * @param args
+	 *
+	 */
+	public static void main(String[] args) {
+		
+		
+		System.out.println("测试一发");
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
